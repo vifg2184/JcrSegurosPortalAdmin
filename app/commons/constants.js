bafa5d586b80f0850c695854c3d82fa56bebebce
vifg2184 <vladimir.fernandez21@gmail.com>
2016-10-20 10:22:05 -0400

    Agregado reporte SA

--- app/commons/constants.js
+++ app/commons/constants.js
@@ -64,7 +64,7 @@ angular.module("App")
         PROXY_URL_INFO_SINIESTROS: environment + "Reportes/getInfoSiniestralidad",
         PROXY_URL_REPORTES_SINIESTRALIDAD: environment + "Reportes/createReportSiniestralidad",
         PROXY_URL_INFO_SA: environment + "Reportes/getSumasAsegurasInfo",
-        PROXY_URL_REPORT_SA: environment + "Reportes/createReportSA"
+        PROXY_URL_REPORT_SA: environment + "Reportes/createReportSA",
 
         /*Proxy Recordatorio*/
         PROXY_URL_RECORDATORIOS: environment + "Recordatorio/sendRecordatorio",
